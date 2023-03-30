84373fc096409b5a8765105abd8f10bf7e2dd7f9
Enova <17408285+Enovale@users.noreply.github.com>
2023-03-29 19:39:47 -0500

    Remove some scalar packs

--- sticker/version.py
+++ sticker/version.py
@@ -1,1 +1,6 @@
-from .get_version import git_tag, git_revision, version, linkified_version
+# Generated in setup.py
+
+git_tag = None
+git_revision = '53d56ea7'
+version = '0.1.0+dev.53d56ea7'
+linkified_version = '0.1.0+dev.[53d56ea7](https://github.com/maunium/stickerpicker/commit/53d56ea7fe77387cdfb5717ae5ba0172f5df47cf)'
